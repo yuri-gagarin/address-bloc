4e96007afb7c9178e7d5381972cabaa2fd54254e
robmadden <rob.madden@bloc.io>
2016-03-02 14:25:39 -0800

    Models: Snippet 3

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,5 +1,25 @@
 require_relative '../models/entry'
 
+# #1
 RSpec.describe Entry do
+  # #2
+  describe "attributes" do
+    # #3
+    it "responds to name" do
+      entry = Entry.new
+      # #4
+      expect(entry).to respond_to(:name)
+    end
 
+    it "responds to phone number" do
+      entry = Entry.new
+      expect(entry).to respond_to(:phone_number)
+    end
+
+    it "responds to email" do
+      entry = Entry.new
+      expect(entry).to respond_to(:email)
+    end
+  end
 end
+
