7340ede39272e75a0cf0355f0bfcbc4eb3f4f77f
robmadden <rob.madden@bloc.io>
2016-03-02 14:52:10 -0800

    Reading CSVs: Snippet 1

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,33 +1,32 @@
 require_relative '../models/address_book'
 
 RSpec.describe AddressBook do
+  # #1
+  let(:book) { AddressBook.new }
+
+  # #2
   describe "attributes" do
     it "responds to entries" do
-      book = AddressBook.new
       expect(book).to respond_to(:entries)
     end
 
     it "initializes entries as an array" do
-      book = AddressBook.new
       expect(book.entries).to be_an(Array)
     end
 
     it "initializes entries as empty" do
-      book = AddressBook.new
       expect(book.entries.size).to eq(0)
     end
   end
 
   describe "#add_entry" do
     it "adds only one entry to the address book" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
       expect(book.entries.size).to eq(1)
     end
 
     it "adds the correct information to entries" do
-      book = AddressBook.new
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       new_entry = book.entries[0]
 
@@ -35,6 +34,19 @@ RSpec.describe AddressBook do
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
+  end
+
+  # Test that AddressBook's .import_from_csv() method is working as expected
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      # #3
+      book.import_from_csv("entries.csv")
+      book_size = book.entries.size
+
+      # Check the size of the entries in AddressBook
+      expect(book_size).to eq 5
+    end
+
   end
 end
 
