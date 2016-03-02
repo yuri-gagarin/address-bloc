a3075889b4f80fbc0956acc0ce7a4b60725831cf
robmadden <rob.madden@bloc.io>
2016-03-02 14:58:23 -0800

    Searching: Snippet 2

--- models/address_book.rb
+++ models/address_book.rb
@@ -33,4 +33,8 @@ class AddressBook
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
+
+  # Search AddressBook for a specific entry by name
+  def binary_search(name)
+  end
 end
