3777fe3afe3b2dd54127e4087a5562383e80c0fb
robmadden <rob.madden@bloc.io>
2016-03-02 14:41:35 -0800

    Models: Snippet 15

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -6,6 +6,16 @@ RSpec.describe AddressBook do
       book = AddressBook.new
       expect(book).to respond_to(:entries)
     end
+
+    it "initializes entries as an array" do
+      book = AddressBook.new
+      expect(book.entries).to be_an(Array)
+    end
+
+    it "initializes entries as empty" do
+      book = AddressBook.new
+      expect(book.entries.size).to eq(0)
+    end
   end
 
 end
