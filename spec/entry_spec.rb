ececf63c320f3d93801b74dc8a35d355cac5209f
robmadden <rob.madden@bloc.io>
2016-03-02 14:34:54 -0800

    Remove callouts from previous snippet: "Models: Snippet 4"

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,23 +1,19 @@
 require_relative '../models/entry'
 
-# #1
 RSpec.describe Entry do
-  # #2
   describe "attributes" do
-    # #3
     it "responds to name" do
-      entry = Entry.new
-      # #4
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:name)
     end
 
     it "responds to phone number" do
-      entry = Entry.new
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:phone_number)
     end
 
     it "responds to email" do
-      entry = Entry.new
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:email)
     end
   end
