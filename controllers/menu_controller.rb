03b96322ed97231812af7c6b873150b199724db9
robmadden <rob.madden@bloc.io>
2016-03-02 14:49:23 -0800

    Menu: Snippet 3

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -20,6 +20,47 @@ class MenuController
 
     # #3
     selection = gets.to_i
-    puts "You picked #{selection}"
+
+    # #7
+    case selection
+      when 1
+        system "clear"
+        view_all_entries
+        main_menu
+      when 2
+        system "clear"
+        create_entry
+        main_menu
+      when 3
+        system "clear"
+        search_entries
+        main_menu
+      when 4
+        system "clear"
+        read_csv
+        main_menu
+      when 5
+        puts "Good-bye!"
+        # #8
+        exit(0)
+      # #9
+      else
+        system "clear"
+        puts "Sorry, that is not a valid input"
+        main_menu
+    end
+  end
+
+  # #10
+  def view_all_entries
+  end
+
+  def create_entry
+  end
+
+  def search_entries
+  end
+
+  def read_csv
   end
 end
