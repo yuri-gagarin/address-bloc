110bb0faa5bba78723c3d642adb3f7b1e5479d78
robmadden <rob.madden@bloc.io>
2016-03-02 14:56:17 -0800

    Reading CSVs: Snippet 6

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,7 +19,15 @@ class AddressBook
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  # #7
   def import_from_csv(file_name)
     # Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+  # #8
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
