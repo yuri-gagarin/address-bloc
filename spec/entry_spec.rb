4a7770d8e32c6b6d284cf1eb0341f87eb3f80dbf
robmadden <rob.madden@bloc.io>
2016-03-02 14:36:26 -0800

    Models: Snippet 9

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -2,33 +2,29 @@ require_relative '../models/entry'
 
 RSpec.describe Entry do
   describe "attributes" do
+    let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
+
     it "responds to name" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:name)
     end
 
     it "reports its name" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry.name).to eq('Ada Lovelace')
     end
 
     it "responds to phone number" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:phone_number)
     end
 
     it "reports its phone_number" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry.phone_number).to eq('010.012.1815')
     end
 
     it "responds to email" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:email)
     end
 
     it "reports its email" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry.email).to eq('augusta.king@lovelace.com')
     end
   end
