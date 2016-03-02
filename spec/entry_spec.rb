fc6a1ddb5af828db6d16d05da9e1511171219622
robmadden <rob.madden@bloc.io>
2016-03-02 14:35:37 -0800

    Models: Snippet 7

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -7,14 +7,29 @@ RSpec.describe Entry do
       expect(entry).to respond_to(:name)
     end
 
+    it "reports its name" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(entry.name).to eq('Ada Lovelace')
+    end
+
     it "responds to phone number" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:phone_number)
     end
 
+    it "reports its phone_number" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(entry.phone_number).to eq('010.012.1815')
+    end
+
     it "responds to email" do
       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expect(entry).to respond_to(:email)
+    end
+
+    it "reports its email" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(entry.email).to eq('augusta.king@lovelace.com')
     end
   end
 end
