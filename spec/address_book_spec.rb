83b42b0eb59872c0fe2efd6c6a31119599040051
robmadden <rob.madden@bloc.io>
2016-03-04 09:31:00 -0800

    Models: Snippet 17

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#add_entry" do
+    it "adds only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      expect(book.entries.size).to eq(1)
+    end
+
+    it "adds the correct information to entries" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      new_entry = book.entries[0]
+
+      expect(new_entry.name).to eq('Ada Lovelace')
+      expect(new_entry.phone_number).to eq('010.012.1815')
+      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+    end
+  end
 end
+
