75882c5c2ea5de316717b614cfc5faf9ca818b10
robmadden <rob.madden@bloc.io>
2016-03-24 11:43:42 -0700

    Remove callouts from previous commit: Searching: Snippet 6

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,17 +31,13 @@ class AddressBook
 
   # Search AddressBook for a specific entry by name
   def binary_search(name)
-    # #1
     lower = 0
     upper = entries.length - 1
 
-    # #2
     while lower <= upper
-      # #3
       mid = (lower + upper) / 2
       mid_name = entries[mid].name
 
-      # #4
       if name == mid_name
         return entries[mid]
       elsif name < mid_name
@@ -51,7 +47,6 @@ class AddressBook
       end
     end
 
-    # #5
     return nil
   end
 end
